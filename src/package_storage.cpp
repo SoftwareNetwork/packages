8357319c30f3e2228d6d7e7100f7019bd7571f31
Egor Pugin <egor.pugin@gmail.com>
2024-06-19 00:27:16 +0300

    Update.

--- src/package_storage.cpp
+++ src/package_storage.cpp
@@ -18,6 +18,36 @@ using path = sw::path;
 using sw::replace_all;
 namespace fs = sw::fs;
 
+auto format_version(auto &&format_string, auto &&version) {
+    auto v = format_string;
+    v = replace_all(v, "{v}", version);
+    v = replace_all(v, "{M}", std::to_string(version[0]));
+    v = replace_all(v, "{m}", std::to_string(version[1]));
+    v = replace_all(v, "{p}", std::to_string(version[2]));
+    if (version[2]) {
+        v = replace_all(v, "{po}", "."s + std::to_string(version[2]));
+    }
+    else {
+        v = replace_all(v, "{po}", {});
+    }
+    if (version.size() > 3) {
+        v = replace_all(v, "{t}", std::to_string(version[3]));
+        if (version[3]) {
+            v = replace_all(v, "{to}", "."s + std::to_string(version[3]));
+        }
+        else {
+            v = replace_all(v, "{to}", {});
+        }
+    }
+    if (version.size() > 4) {
+        v = replace_all(v, "{v5}", std::to_string(version[4]));
+    }
+    if (version.size() > 5) {
+        v = replace_all(v, "{v6}", std::to_string(version[5]));
+    }
+    return v;
+}
+
 struct git_like_storage {
     using hash_type = crypto::sha3<256>;
 };
@@ -36,13 +66,13 @@ struct git_remote {
 
         auto it = obj.tag_commits.find(tag);
         if (it == obj.tag_commits.end()) {
-            throw std::runtime_error{"tag not found"};
+            throw std::runtime_error{ "tag not found" };
         }
-        return std::string{it->second};
+        return std::string{ it->second };
     }
-    bool has_tag(this auto &&obj, const std::string &tag) {
+    bool has_version(this auto &&obj, const std::string &version) {
         obj.read_refs();
-        return obj.tag_commits.contains(tag);
+        return obj.tag_commits.contains(version);
     }
     void read_refs(this auto &&obj) {
         if (!obj.refs.empty()) {
@@ -58,10 +88,10 @@ struct git_remote {
         c.run();
         obj.refs = c.out.get<std::string>();
         if (!obj.refs.starts_with("001e#"sv)) {
-            throw std::runtime_error{"bad git response"};
+            throw std::runtime_error{ "bad git response" };
         }
-        for (auto &&[b,e] : std::views::split(obj.refs, "\n"sv) | std::views::drop(2)) {
-            std::string_view sv{b,e};
+        for (auto &&[b, e] : std::views::split(obj.refs, "\n"sv) | std::views::drop(2)) {
+            std::string_view sv{ b,e };
             auto s = " refs/tags/"sv;
             auto p = sv.find(s);
             if (p != -1) {
@@ -84,31 +114,114 @@ struct tag {
     std::string value;
 
     auto operator()(const sw::package_version &version) const {
-        auto v = value;
-        v = replace_all(v, "{v}", version);
-        v = replace_all(v, "{M}", std::to_string(version[0]));
-        v = replace_all(v, "{m}", std::to_string(version[1]));
-        v = replace_all(v, "{p}", std::to_string(version[2]));
-        if (version[2]) {
-            v = replace_all(v, "{po}", "."s + std::to_string(version[2]));
-        } else {
-            v = replace_all(v, "{po}", {});
+        return tag{ format_version(value, version) };
+    }
+};
+
+template <typename PackageStorage>
+struct software {
+    PackageStorage &storage;
+    // source
+
+    software(PackageStorage &storage, auto id) : storage{ storage } {
+    }
+    auto add(auto &&v) {
+        v.software = this;
+        return std::move(v);
+    }
+    auto add_repository(auto &&) {
+    }
+    auto add_source(auto &&) {
+    }
+    auto operator+=(auto &&) {
+    }
+};
+
+struct package_storage {
+    path root;
+
+    package_storage(const path &root) : root{ root } {
+        fs::create_directories(root / "tmp");
+    }
+    void add(auto &&remote) {
+        auto url = remote.url();
+
+        int a = 5;
+        a++;
+    }
+
+    auto add_software(auto && ... args) {
+        return software{ *this, args... };
+    }
+};
+
+struct default_version_incrementer {
+    std::set<sw::package_version> versions;
+
+    default_version_incrementer(sw::package_version base) {
+        for (auto &&e : std::get<sw::package_version::number_version>(base.version).elements.value | std::views::reverse) {
+            ++e;
+            versions.insert(base);
+            e = 0;
         }
-        if (version.size() > 3) {
-            v = replace_all(v, "{t}", std::to_string(version[3]));
-            if (version[3]) {
-                v = replace_all(v, "{to}", "."s + std::to_string(version[3]));
-            } else {
-                v = replace_all(v, "{to}", {});
+    }
+};
+
+template <typename Source>
+struct version_list {
+    Source &src;
+    std::string version_format;
+    std::set<sw::package_version> versions;
+    sw::package_version last_ver;
+
+    auto begin() { return versions.begin(); }
+    auto end() { return versions.end(); }
+    void add(const std::initializer_list<int> &v) {
+        versions.insert(v);
+        last_ver = v;
+    }
+    auto operator+=(const std::initializer_list<int> &v) {
+        add(v);
+    }
+    bool check_next(int recursion = 0, std::source_location sl = std::source_location::current()) {
+        bool result{};
+        default_version_incrementer i{ last_ver };
+
+        // use reverse and add only latest versions for now
+        for (auto &&v : i.versions | std::views::reverse) {
+            if (!versions.contains(v) && src.has_version(format_version(version_format, v))) {
+                versions.insert(v);
+                last_ver = v;
+                result = true;
+                if (check_next(recursion + 1, sl)) {
+                }
+                // unconditionally
+                break;
             }
         }
-        if (version.size() > 4) {
-            v = replace_all(v, "{v5}", std::to_string(version[4]));
+        if (recursion) {
+            return result;
         }
-        if (version.size() > 5) {
-            v = replace_all(v, "{v6}", std::to_string(version[5]));
+        if (!result) {
+            return result;
         }
-        return tag{v};
+        static int added_lines = 0;
+        auto f = sw::read_file(sl.file_name());
+        auto [b, e] = *(std::views::split(f, "\n"sv) | std::views::drop(sl.line() - 1 + added_lines++)).begin();
+        // we expect this line to be (with spaces in the beginning) '    version_list_variable_name.check_next();'
+        auto line = std::string_view{ b, e };
+        auto pos = &line[0] - &f[0];
+        auto identifier_start = f.find_first_not_of(' ', pos);
+        auto spaces = identifier_start - pos;
+        std::string space(spaces, ' ');
+        auto identifier_end = f.find_first_of('.', identifier_start);
+        auto id = f.substr(identifier_start, identifier_end - identifier_start);
+        f = f.substr(0, pos) + std::format("{}{} += {};\n", space, id, last_ver.to_string_initializer_list()) + f.substr(pos);
+        sw::write_file(sl.file_name(), f);
+        // we don't need to rebuild and run the file again because
+        // new version is in the version list now,
+        // so it will be downloaded
+        return result;
     }
 };
 
@@ -116,8 +229,9 @@ struct tag {
 
 struct github_repository : git_remote {
     std::string user_repo;
+    software<package_storage> *software{};
 
-    github_repository(auto &&user_repo) : user_repo{user_repo} {
+    github_repository(auto &&user_repo) : user_repo{ user_repo } {
     }
     github_repository(auto &&user, auto &&repo) {
         user_repo += user;
@@ -135,167 +249,112 @@ struct github_repository : git_remote {
                 return value;
             }
         };
-        return x{url() + std::format("/archive/{}.tar.gz", t.value)};
+        return x{ url() + std::format("/archive/{}.tar.gz", t.value) };
     }
     auto operator()(const tag &t) {
-        return operator()(commit{tag_hash(t.value)});
+        return operator()(commit{ tag_hash(t.value) });
         //return x{url() + std::format("/archive/refs/tags/{}.tar.gz", t.value)};
+    }
+
+    auto add_version_list(const tag &version_format) {
+        return version_list<github_repository>{*this, version_format.value};
     }
 };
 auto operator""_github(const char *s, size_t len) {
-    return github_repository{std::string_view{s,s+len}};
+    return github_repository{ std::string_view{s,s + len} };
 }
 
-template <typename PackageStorage>
-struct software {
-    PackageStorage &storage;
-    // source
-
-    software(PackageStorage &storage, auto id) : storage{storage} {
-    }
-    auto add(auto &&) {
-    }
-    auto add_repository(auto &&) {
-    }
-
-    auto operator+=(auto &&) {
-    }
-};
-
-struct package_storage {
-    path root;
-
-    package_storage(const path &root) : root{root} {
-        fs::create_directories(root / "tmp");
-    }
-    void add(auto &&remote) {
-        auto url = remote.url();
-
-        int a = 5;
-        a++;
-    }
-
-    auto add_software(auto && ... args) {
-        return software{*this, args...};
-    }
-};
-
 struct versioned_url {
+    std::string url;
+    software<package_storage> *software{};
 
-};
+    versioned_url(const std::string_view &url) : url{ url } {
+    }
+    versioned_url(const std::string &url) : url{ url } {
+    }
+    auto add_version_list() {
+        return version_list<versioned_url>{*this, url};
+    }
 
-struct default_version_incrementer {
-    std::set<sw::package_version> versions;
-
-    default_version_incrementer(sw::package_version base) {
-        for (auto &&e : std::get<sw::package_version::number_version>(base.version).elements.value | std::views::reverse) {
-            ++e;
-            versions.insert(base);
-            e = 0;
-        }
+    bool has_version(auto &&v) const {
+        sw::raw_command c;
+        c += sw::resolve_executable("curl"), "-ILf", v;
+        c.out.close();
+        c.err.close();
+        std::error_code ec;
+        c.run(ec);
+        return !ec;
     }
 };
-
-struct version_list {
-    std::set<sw::package_version> versions;
-    sw::package_version last_ver;
-
-    auto begin() {return versions.begin();}
-    auto end() {return versions.end();}
-    void add(const std::initializer_list<int> &v) {
-        versions.insert(v);
-        last_ver = v;
-    }
-    auto operator+=(const std::initializer_list<int> &v) {
-        add(v);
-    }
-    bool check_next(auto &&repo, auto &&tag, int recursion = 0, std::source_location sl = std::source_location::current()) {
-        bool result{};
-        default_version_incrementer i{last_ver};
-
-        // use reverse and add only latest versions for now
-        for (auto &&v : i.versions | std::views::reverse) {
-            if (!versions.contains(v) && repo.has_tag(tag(v).value)) {
-                versions.insert(v);
-                last_ver = v;
-                result = true;
-                if (check_next(repo, tag, recursion + 1, sl)) {
-                }
-                // unconditionally
-                break;
-            }
-        }
-        if (recursion) {
-            return result;
-        }
-        if (!result) {
-            return result;
-        }
-        static int added_lines = 0;
-        auto f = sw::read_file(sl.file_name());
-        auto [b,e] = *(std::views::split(f, "\n"sv) | std::views::drop(sl.line() - 1 + added_lines++)).begin();
-        auto line = std::string_view{b, e};
-        auto pos = &line[0] - &f[0];
-        auto non_space = f.find_first_not_of(' ', pos);
-        auto spaces = non_space - pos;
-        std::string space(spaces, ' ');
-        f = f.substr(0, pos) + std::format("{}vl.add({});\n", space, last_ver.to_string_initializer_list()) + f.substr(pos);
-        sw::write_file(sl.file_name(), f);
-        // we dont need to rebuild and run the file again because
-        // new version is in the version list now,
-        // so it will be downloaded
-        return result;
-    }
-};
+auto operator""_url(const char *s, size_t len) {
+    return versioned_url{ std::string_view{s,s + len} };
+}
 
 int main1(int argc, char *argv[]) {
     if (argc == 1) {
         std::cerr << std::format("usage: {} storage_dir\n", argv[0]);
         return 1;
     }
-    package_storage ps{argv[1]};
+    package_storage ps{ argv[1] };
 
-    tag vv{"v{v}"};
-    tag vmmpo{"v{M}.{m}{po}"};
+    tag vv{ "v{v}" };
+    tag vmmpo{ "v{M}.{m}{po}" };
 
     //auto ??? = repo.add_prefix("prefix or some id");
     //auto org = repo.add_organization("prefix or some id");
 
+    // zlib
     {
-        auto zlib = ps.add_software("org.zlib");
-        zlib += "madler/zlib"_github;
-        zlib.add("madler/zlib"_github);
-        zlib.add_repository("madler/zlib"_github);
+        //auto org = repo.add_organization("org.madler");
+        //org.add_software("");
+        auto zlib = ps.add_software("org.madler");
+        auto gh = zlib.add("madler/zlib"_github);
+        auto vl = gh.add_version_list(vmmpo);
+        vl += {1, 2, 3};
+        vl += {1, 3, 1};
+        vl.check_next();
 
-        auto gh = "madler/zlib"_github;
-
-        version_list vl;
-        vl += {1,2,3};
-        vl += {1,3,1};
-        vl.check_next(gh, vmmpo);
-
-        for (auto &&v : vl)
-        {
+        for (auto &&v : vl) {
             //madler_zlib.download(vmmpo(v));
             //madler_zlib(vmmpo({1,3}));
             //auto pkg = repo.add(madler_zlib(vmmpo({1,2,13})));
             //repo.add(madler_zlib(vmmpo({1,3,1})));
         }
     }
+    // png
     {
-        github_repository ghr{"glennrp/libpng"};
-        version_list vl;
-        //vl.add({1,0,5}); separate cfg in sw
-        vl.add({1,6,35});
-        vl.add({1,6,43});
-        vl.check_next(ghr, vv);
-        for (auto &&v : vl)
-        {
+        auto png = ps.add_software("org.glennrp");
+        auto gh = png.add("glennrp/libpng"_github);
+        auto vl_old = gh.add_version_list(vv);
+        vl_old += {1, 0, 5}; // separate cfg in sw
+        vl_old += {1, 0, 11};
+        vl_old.check_next();
+        for (auto &&v : vl_old) {
             //madler_zlib.download(vmmpo(v));
             //madler_zlib(vmmpo({1,3}));
             //auto pkg = repo.add(madler_zlib(vmmpo({1,2,13})));
             //repo.add(madler_zlib(vmmpo({1,3,1})));
         }
+
+        auto vl = gh.add_version_list(vv);
+        vl += {1, 6, 35};
+        vl += {1, 6, 43};
+        vl.check_next();
+        for (auto &&v : vl) {
+            //madler_zlib.download(vmmpo(v));
+            //madler_zlib(vmmpo({1,3}));
+            //auto pkg = repo.add(madler_zlib(vmmpo({1,2,13})));
+            //repo.add(madler_zlib(vmmpo({1,3,1})));
+        }
+    }
+    // glib
+    {
+        auto glib = ps.add_software("org.gnome.glib");
+        auto url = glib.add("https://ftp.gnome.org/pub/gnome/sources/glib/{M}.{m}/glib-{v}.tar.xz"_url);
+        auto vl = url.add_version_list();
+        vl += {2, 80, 0}; // separate cfg in sw
+        vl += {2, 80, 3};
+        vl.check_next();
     }
 
     return 0;
